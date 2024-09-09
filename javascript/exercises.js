7fc3a09498c840547a5e4363b78ae0984e3da41a
Brian Bowers <bmbowers22@gmail.com>
2024-09-08 23:50:35 -0700

    working on problems

--- javascript/exercises.js
+++ javascript/exercises.js
@@ -1,3 +1,4 @@
+import { error } from "node:console"
 import { open } from "node:fs/promises"
 
 export function change(amount) {
@@ -16,11 +17,40 @@ export function change(amount) {
 }
 
 // Write your first then lower case function here
-
+export function firstThenLowerCase(array, predicate) {
+  const first = array.find(predicate)
+  return first?.toLowerCase()
+}
 // Write your powers generator here
-
+export function* powersGenerator({ ofBase, upTo }) {
+  var exponent = 0
+  var value = ofBase ** exponent
+  while (value <= upTo) {
+    yield ofBase ** exponent
+    exponent++
+    value = ofBase ** exponent
+  }
+}
 // Write your say function here
-
+export function say(string) {
+  if (string != undefined) {
+    return (newString) => {
+      if (newString != undefined) {
+        return say(string + " " + newString)
+      } else return string
+    }
+  } else return ""
+}
 // Write your line count function here
+export async function meaningfulLineCount(fileName) {
+  const file = await open(fileName)
+  var count = 0
 
+  for await (const line of file.readLines()) {
+    if (!line.trim().startsWith("#") && line.trim().length != 0) {
+      count += 1
+    }
+  }
+  return count
+}
 // Write your Quaternion class here
