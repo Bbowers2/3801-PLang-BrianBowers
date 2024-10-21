986405aa9e61a19e56d910826b41f562ac2524ac
Brian Bowers <bmbowers22@gmail.com>
2024-10-21 00:53:46 -0700

    first three tests passing and outline for next two problems

--- typescript/exercises.ts
+++ typescript/exercises.ts
@@ -13,12 +13,60 @@ export function change(amount: bigint): Map<bigint, bigint> {
   return counts
 }
 
-// Write your first then apply function here
+export function firstThenApply<T, U>(items: T[], predicate: (item: T) => boolean, consumer: (item: T) => U): U | undefined {
+  var first_element: T | undefined = items.find(predicate)
 
-// Write your powers generator here
+  return first_element !== undefined ? consumer(first_element): undefined
+}
 
-// Write your line count function here
 
-// Write your shape type and associated functions here
+export function* powersGenerator(base: bigint): Generator<bigint>{
+  for (let exponentiation = 1n; ; exponentiation *= base)
+    yield exponentiation
+}
+
+export async function meaningfulLineCount(filename: string){
+  let count: number = 0
+  const file = await open(filename, "r")
+  for await (const line of file.readLines()){
+    const trimmed: string = line.trim()
+    if (trimmed && !trimmed.startsWith("#")) {
+      count++
+    }
+  }
+  return count
+}
+
+interface Sphere {
+  kind: "Sphere"
+  radius: number
+}
+
+interface Box {
+  kind: "Box"
+  width: number
+  height: number
+  depth: number
+}
+
+export type Shape = Sphere | Box
+
+export function surfaceArea(shape: Shape): number {return 0}
+export function volume(shape: Box): number {return 0}
+
 
 // Write your binary search tree implementation here
+export interface BinarySearchTree<T>{
+  size(): number
+  insert(value: T): BinarySearchTree<T>
+  contains(value: T): boolean
+  inorder(): Iterable<T>
+}
+
+export class Empty<T> implements BinarySearchTree<T> {
+
+}
+
+export class BinarySearchTree<T> implements BinarySearchTree<T> {
+  
+}
